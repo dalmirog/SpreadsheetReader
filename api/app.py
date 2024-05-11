111053ff6f787e93ed8b45a359de31815c777501
dalmiro grañas <dalmiro.granas@gmail.com>
2024-05-11 02:03:57 -0300

    api module also working

---
+++ api/app.py
@@ -0,0 +1,19 @@
+# import spreadsheetreader.SpreadsheetReader as SpreadsheetReader
+from spreadsheetreader import Reader
+from flask import Flask, jsonify, request
+import json
+from json import JSONEncoder
+
+# creating a Flask app 
+app = Flask(__name__)
+
+reader = Reader(
+      spreadsheetId= "1DV-eajMmYxksh1JFdn-LJQ_y8RgdbFBh_-u-vF1hVXA",
+      spreadsheetName= "Data",
+      columnRange="A2:C")
+
+@app.route('/', methods = ['GET']) 
+def home():
+    values = reader.execute()
+    print(values)
+    return 'OK', 200
