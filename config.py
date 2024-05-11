35b56f79739e00e83aff898e93e2600f4910201a
dalmiro grañas <dalmiro.granas@gmail.com>
2024-05-11 03:25:30 -0300

    config and launch.json working

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
 
 class Config:
-    SPREADSHEETID = "1DV-eajMmYxksh1JFdn-LJQ_y8RgdbFBh_-u-vF1hVXA"
-    SPREADSHEETNAME = "Data"
-    COLUMNRANGE ="A2:C"
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SPREADSHEETID = os.environ.get('SPREADSHEETID')
+    SPREADSHEETNAME = os.environ.get('SPREADSHEETNAME')
+    COLUMNRANGE = os.environ.get('COLUMNRANGE')
 
 
